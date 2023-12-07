d636bbc4a808a6e1b97e50fab2278b54b5a4a4bf
frombread <gon03004@gmail.com>
2023-12-07 17:36:13 +0900

    fix socket

--- src/main/java/com/example/lastproject/config/WebSocketConfig.java
+++ src/main/java/com/example/lastproject/config/WebSocketConfig.java
@@ -11,12 +11,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config){
-        config.enableSimpleBroker("/send");
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config){
+//        config.enableSimpleBroker("/send");
+//        config.setApplicationDestinationPrefixes("/app");
+//    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
--- src/main/java/com/example/lastproject/controller/MessageController.java
+++ src/main/java/com/example/lastproject/controller/MessageController.java
@@ -5,38 +5,48 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
+import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-@Component
+@Controller
 @ServerEndpoint("/ws/chat/{courseId}")
 public class MessageController {
     private final SimpMessagingTemplate messagingTemplate;
-    private static final List<Session> sessions = new ArrayList<>();
-
+    private static final List<Session> sessions = new CopyOnWriteArrayList<>();
+    public MessageController() {
+        this.messagingTemplate = null; // 또는 null이 아닌 기본 값을 설정해 주세요.
+    }
     @Autowired
     public MessageController(SimpMessagingTemplate messagingTemplate) {
         this.messagingTemplate = messagingTemplate;
     }
 
     @OnOpen
-    public void open(Session newUser, @PathParam("courseId") String courseId) {
+    public void open(Session newUser) {
         System.out.println("connected");
         sessions.add(newUser);
         System.out.println(newUser.getId());
+    }
+
+    @OnClose
+    public void onClose(Session userSession) {
+        System.out.println("Disconnected: " + userSession.getId());
+        sessions.remove(userSession);
     }
 
     @OnMessage
     public void getMsg(Session receiveSession, String msg, @PathParam("courseId") String courseId) throws IOException {
         for (Session session : sessions) {
             if (session.isOpen()) {
-                session.getBasicRemote().sendText(courseId +" : "+ msg);
+                session.getBasicRemote().sendText(msg);
             }
         }
     }
